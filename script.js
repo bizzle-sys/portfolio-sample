5165453485ea96afd4ad669dfa2deceffc3fd6cb
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-02-26 12:26:22 +1100

    minor changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sun = document.getElementById("theme-icon")
 
 sun.addEventListener('click', (e) => {
-    document.body.classList.toggle("dark-theme ")
+    document.body.classList.toggle("dark-theme")
 })
